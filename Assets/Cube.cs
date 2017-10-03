4eb1a89e44634246dd8bcbca53438d19770135c2
DESKTOP-BMRPB8E\johnp <johnpatrick415@gmail.com>
2017-10-03 12:27:56 -0400

    more tut

---
+++ Assets/Cube.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Cube : MonoBehaviour {
+
+	//dimension for the cube and number of vertices?
+	public int xSize, ySize, zSize;
+
+	private Mesh mesh;
+	private Vector3[] vertices;
+
+	private void Awake(){
+		StartCoroutine(Generate());
+	}
+
+	private IEnumerator Generate(){
+		GetComponent<MeshFilter> ().mesh = mesh = new Mesh ();
+		mesh.name = "Procedural Cube";
+		WaitForSeconds wait = new WaitForSeconds (0.05f);
+
+		int cornerVertices = 8;
+		int edgeVertices = (xSize + ySize + zSize - 3) * 4;
+		int faceVertices = (
+		                       (xSize - 1) * (ySize - 1) +
+		                       (xSize - 1) * (zSize - 1) +
+		                       (ySize - 1) * (zSize - 1)) * 2;
+		vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+
+		int v = 0;
+		for (int x = 0; x <= xSize; x++) {
+			vertices [v++] = new Vector3 (x, 0, 0);
+			yield return wait;
+		}
+		for (int z = 0; z <= zSize; z++) {
+			vertices [v++] = new Vector3 (xSize, 0, z);
+			yield return wait;
+		}
+
+
+
+
+	}
+
+	private void OnDrawGizmos(){
+		if (vertices == null) {
+			return;
+		}
+		Gizmos.color = Color.black;
+		for (int i = 0; i < vertices.Length; i++) {
+			Gizmos.DrawSphere (vertices [i], 0.1f);
+		}
+	}
+}
--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -10,33 +10,45 @@ public class Grid : MonoBehaviour {
 	private Vector3[] vertices;
 
 	private void Awake () {
-		StartCoroutine(Generate());
+		Generate();
 	}
 
-	private IEnumerator Generate () {
-		WaitForSeconds wait = new WaitForSeconds (0.05f);
+	private void Generate () {
+		//WaitForSeconds wait = new WaitForSeconds (0.05f);
 
 		GetComponent<MeshFilter> ().mesh = mesh = new Mesh ();
 		mesh.name = "Procedural Grid";
 
 		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		Vector2[] uv = new Vector2[vertices.Length];
+		Vector4[] tangents = new Vector4[vertices.Length];
+		Vector4 tangent = new Vector4 (1f, 0f, 0f, -1f);
+
 
 		for (int i = 0, y = 0; y <= ySize; y++) {
 			for (int x = 0; x <= xSize; x++, i++) {
 				vertices [i] = new Vector3 (x, y);
-				yield return wait;
+				uv [i] = new Vector2 ((float)x / xSize, (float)y / ySize);
+				tangents [i] = tangent;
+				//yield return wait;
 			}
 		}
 		mesh.vertices = vertices;
+		mesh.uv = uv;
+		mesh.tangents = tangents;
 
-		int[] triangles = new int[xSize * 6];
-		triangles [0] = 0;
-		triangles [1] = xSize + 1;
-		triangles [2] = 1;
-		triangles [3] = triangles[2] = 1;
-		triangles [4] = triangles[1] = xSize + 1;
-		triangles [5] = xSize + 2;
+		int[] triangles = new int[xSize * ySize * 6];
+		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+				triangles [ti] = vi;
+				triangles [ti + 3] = triangles [ti + 2] = vi + 1;
+				triangles [ti + 4] = triangles [ti + 1] = vi + xSize + 1;
+				triangles [ti + 5] = vi + xSize + 2;
+				//yield return wait;
+			}
+		}
 		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
 	}
 
 	private void OnDrawGizmos(){
