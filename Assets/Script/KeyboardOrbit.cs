93aebdc4bd52944536b1f573eec3c5468dd58683
john san diego <johnpatrick415@gmail.com>
2017-11-11 23:02:36 -0500

    Added KeyboardOrbit

---
+++ Assets/Script/KeyboardOrbit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardOrbit : MonoBehaviour {
+
+	public float speed = 50f;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (Vector3.up * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (Vector3.down * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Rotate (Vector3.left * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Rotate (Vector3.right * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.Q)) {
+			transform.Rotate (Vector3.forward * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			transform.Rotate (Vector3.back * Time.deltaTime * speed);
+		}
+	}
+}
