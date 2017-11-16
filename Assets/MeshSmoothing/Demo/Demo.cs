eb789711643296e5b5691591d72301c084337eaf
DESKTOP-BMRPB8E\johnp <johnpatrick415@gmail.com>
2017-11-16 13:56:14 -0500

    added Dropdown and button for smoothing the object. trying to figure out how to smooth just a spot

--- Assets/MeshSmoothing/Demo/Demo.cs
+++ Assets/MeshSmoothing/Demo/Demo.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 namespace mattatz.MeshSmoothingSystem.Demo {
 
 	[RequireComponent (typeof(MeshRenderer))]
 	[RequireComponent (typeof(MeshFilter))]
 	public class Demo : MonoBehaviour {
+
+		public Dropdown SmoothAlgorithmValue;
+		public Button ActivateSmooth;
 
 		[System.Serializable] 
 		enum FilterType {
@@ -23,7 +27,7 @@ namespace mattatz.MeshSmoothingSystem.Demo {
 
 		MeshFilter _filter;
 
-		[SerializeField, Range(0f, 1f)] float intensity = 0.5f;
+		[SerializeField, Range(0f, 1f)] float intensity = 0.0f;
 		[SerializeField] FilterType type;
 		[SerializeField, Range(0, 20)] int times = 3;
 		[SerializeField, Range(0f, 1f)] float hcAlpha = 0.5f;
@@ -32,20 +36,24 @@ namespace mattatz.MeshSmoothingSystem.Demo {
 		void Start () {
 			var mesh = filter.mesh;
 			filter.mesh = ApplyNormalNoise(mesh);
-
-			switch(type) {
-			case FilterType.Laplacian:
-				filter.mesh = MeshSmoothing.LaplacianFilter(filter.mesh, times);
-				break;
-			case FilterType.HC:
-				filter.mesh = MeshSmoothing.HCFilter(filter.mesh, times, hcAlpha, hcBeta);
-				break;
-			}
+			ActivateSmooth.onClick.AddListener (LaplacianSmooth);
+//			switch(type) {
+//			case FilterType.Laplacian:
+//				filter.mesh = MeshSmoothing.LaplacianFilter(filter.mesh, times);
+//				break;
+//			case FilterType.HC:
+//				filter.mesh = MeshSmoothing.HCFilter(filter.mesh, times, hcAlpha, hcBeta);
+//				break;
+//			}
 		}
 		
 		 void Update () {
-			if(Input.GetKeyDown(KeyCode.T)){
+			if(Input.GetKeyDown(KeyCode.L)){
 				filter.mesh = MeshSmoothing.LaplacianFilter (filter.mesh, times);
+				return;
+			}
+			if(Input.GetKeyDown(KeyCode.H)){
+				filter.mesh = MeshSmoothing.HCFilter (filter.mesh, times);
 				return;
 			}
 		}
@@ -60,6 +68,29 @@ namespace mattatz.MeshSmoothingSystem.Demo {
 			mesh.vertices = vertices;
 
 			return mesh;
+		}
+		//drop down for smoother. assigns which smoother to use for the button
+		public void SmoothChooser(){
+			switch (SmoothAlgorithmValue.value) {
+			case 0:
+				ActivateSmooth.onClick.AddListener (LaplacianSmooth);
+				break;
+			case 1:
+				ActivateSmooth.onClick.AddListener (HumphreySmooth);
+				break;
+			default:
+				ActivateSmooth.onClick.AddListener (LaplacianSmooth);
+				break;
+			}
+		}
+
+		//function for the button to use
+		public void LaplacianSmooth(){
+			filter.mesh = MeshSmoothing.LaplacianFilter (filter.mesh, times);
+		}
+
+		public void HumphreySmooth(){
+			filter.mesh = MeshSmoothing.HCFilter (filter.mesh, times);
 		}
 
 	}
