7ab68d87c12393df14fb9c6c7439d5bc9ae49d40
john san diego <johnpatrick415@gmail.com>
2017-11-28 16:32:51 -0500

    added undo and redo script and saving and loading scripts

---
+++ Assets/Script/SavingAndLoading.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace sandiegoJohn.VRsculpting{
+	
+	public class SavingAndLoading : MonoBehaviour {
+
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+
+}
---
+++ Assets/Script/UndoAndRedo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace sandiegoJohn.VRsculpting{
+	
+	public class UndoAndRedo : MonoBehaviour {
+
+		Stack<Vector3[]> UndoStack = new Stack<Vector3[]>(); //a vector3 array stack
+		Mesh CurrentMesh; 
+
+		// Use this for initialization
+		void Start () {
+			CurrentMesh = GetComponent<MeshFilter> ().mesh;
+			UndoStack.Push (CurrentMesh.vertices);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			//push the current mesh to stack once the mousebutton is pressed up
+			if (Input.GetMouseButtonUp (0)) {
+				PushToStack ();
+			}
+			//if keyboard key p is pressed pop the current mesh from the stack
+			if (Input.GetKeyDown (KeyCode.P)) {
+				UndoPaint ();
+			}
+		}
+		//Push the current mesh vertices in the stack
+		void PushToStack(){
+			UndoStack.Push (CurrentMesh.vertices);
+		}
+		//checks to make sure their is something in the stack if not return
+		//otherwise, pop the top of the stack and assign value to current mesh vertices
+		//recalculate normals and bounds.
+		void UndoPaint(){
+			if (UndoStack.Count != 0) {
+				CurrentMesh.vertices = UndoStack.Pop ();
+				CurrentMesh.RecalculateNormals ();
+				CurrentMesh.RecalculateBounds ();
+			} else
+				return;
+
+		
+
+		}
+	}
+}
